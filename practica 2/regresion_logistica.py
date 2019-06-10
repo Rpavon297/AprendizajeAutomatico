c5755774cb3be9ef1a57355b28e42510f8d7796d
Roberto <rpbc297@gmail.com>
2019-06-10 13:17:50 +0200

    Funcionanding

--- practica 2/regresion_logistica.py
+++ practica 2/regresion_logistica.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
@@ -18,14 +19,7 @@ Y = np.array([])
 
 
 def sigmoid(z):
-    ret = np.array([])
-    for row in z:
-        r = np.array([])
-        for elem in row:
-            aux = 1.0 / (1.0 + math.exp(float(-elem)))
-            r = np.append(r, aux)
-        ret = np.append(ret, r)
-    return ret
+    return 1 / (1 + np.exp(-z))
 
 
 def h(params, X):
@@ -40,59 +34,73 @@ def h(params, X):
 
     return sigmoid(hipot)
 
+def costvector(theta,X, y):
+    hipTX = sigmoid(np.dot(X, theta))
 
-def h(params, X):
-    paramet = np.dot(X, params)
-
-
-def costvector(X, Y, params):
-    logs = np.array([])
-    ilogs = np.array([])
-
-    for row in X:
-        s = sigmoid([[np.dot(row, params)]])
-
-        l = math.log(s[0][0])
-        il = math.log(1 - s[0][0])
-
-        logs = np.append(logs, l)
-        ilogs = np.append(ilogs, il)
-
-    logs = np.transpose(logs)
-    ilogs = np.transpose(ilogs)
-
-    ones = np.dot(logs, Y)
-    zeros = np.dot(ilogs, np.ones(len(Y)) - Y)
-    return -1.0 / (len(X)) * (ones + zeros)
+    return - (np.dot(y, np.log(hipTX).T) + np.dot(np.log(1 - hipTX).T, (1 - y))) / len(y)
 
 
-def gradiante(X, Y, params):
-    s = sigmoid([[np.dot()]])
+def gradiante(theta,X, y):
+    hipTX = sigmoid(np.dot(X, theta))
 
-    return 1.0 / len(X)
+    return np.dot(X.T, (hipTX -y)) / len(y)
+
+def mostrar(X, Y):
+    pos = np.where(Y == 1)
+    posn = np.where(Y == 0)
+
+    plt.scatter(X[pos, 0], X[pos, 1], marker='.', c='g')
+    plt.scatter(X[posn, 0], X[posn, 1], marker='x', c='r')
 
 
-with open("ex2data1.csv") as file:
-    csvr = csv.reader(file, delimiter=',')
+def mostrarSol(X, Y, theta):
+    plt.figure()
+    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
+    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
 
-    for linea in csvr:
-        X1 = np.append(X1, float(linea[0]))
-        X2 = np.append(X2, float(linea[1]))
-        Y = np.append(Y, float(linea[2]))
+    mostrar(X, Y)
 
-X = np.array([X1, X2])
+    h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)), xx1.ravel(), xx2.ravel()].dot(theta))
+    h = h.reshape(xx1.shape)
+    plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
 
-pos = np.where(Y == 1)
-posn = np.where(Y == 0)
-plt.scatter(X[0, pos], X[1, pos], marker='.', c='g')
-plt.scatter(X[0, posn], X[1, posn], marker='x', c='r')
+    plt.show()
 
-X = [[1, 2, 3, 22, 5]]
-Y= [10]
-Z = [[2,2,3],[2,5,7]]
+
+file = open("ex2data1.csv", "r")
+
+datos = file.read().replace("\n",",").split(",")
+file.close()
+
+del datos[-1]
+
+i = 0
+
+while i < len(datos):
+    X1 = np.append(X1, float(datos[i]))
+    X2 = np.append(X2, float(datos[i+1]))
+    Y = np.append(Y, float(datos[i+2]))
+
+    i = i + 3
+
+Xp = np.array([X1,X2])
+Xp = np.transpose(Xp)
+X = np.hstack((np.ones((Xp.shape[0],1)), Xp))
+params = np.zeros(X.shape[1])
 
 res = sigmoid(X)
-print(res)
+cost = costvector(params,X,Y)
+grad = gradiante(params,X,Y)
 
-plt.title(res)
-plt.show()
+print(X.shape, Y.shape, params.shape)
+print(cost)
+print(grad)
+
+result = opt.fmin_tnc(costvector, params, gradiante, args=(X,Y))
+params_opt = result[0]
+
+coste_opt = costvector(params_opt, X, Y)
+print(coste_opt)
+
+mostrarSol(Xp,Y,params_opt)
